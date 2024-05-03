db7225732dddb998138b627a7f0f40c2f1cde71e
Sherm1337 <162771748+Sherm1337@users.noreply.github.com>
2024-05-02 20:32:36 -0500

    Modify game function
    To Fix:
    - Computer turn executes after gameover sequence
    - Title uncentered on mobile

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameModule =  {
-    createGameBoard: function() {
+    createGameBoard: (function() {
         const mainContainer = document.querySelector(".main-container");
         (function createGameBoardContainer() {
             const gameBoard = document.createElement("div");
@@ -12,40 +12,83 @@ const gameModule =  {
                 gameBoard.appendChild(newTile);
                 //Mark Tile & Push to player.tilesPlayed event listener below
                 newTile.addEventListener("click", () => {
-                        newTile.textContent = gameModule.player.marker;
-                        newTile.id = newTile.getAttribute("id");
-                        gameModule.player.tilesPlayed.push(parseInt(newTile.id));
-                })
+                    if(newTile.textContent == ""){
+                    newTile.textContent = gameOperations.player1.marker;
+                    newTile.id = newTile.getAttribute("id");
+                    gameOperations.player1.tilesPlayed.push(parseInt(newTile.id));
+                    gameOperations.roundCount++;
+                    if (gameOperations.roundCount < 5){
+                        gameOperations.gameOver();
+                        computerTurn();
+                    } else if (gameOperations.roundCount == 5){
+                        gameOperations.gameOver();
+                    }
+                }});
+                computerTurn = function() {
+                    let computerChoice = Math.trunc(Math.random()*9) + 1;
+
+                    tileChoice = document.getElementById(`${computerChoice}`);
+                    if(tileChoice.textContent === gameOperations.player1.marker ||
+                        tileChoice.textContent === gameOperations.computer.marker){
+                            computerChoice = Math.trunc(Math.random()*9) + 1;
+                            computerTurn();
+                    } else if (tileChoice.textContent === ""){
+                    gameOperations.computer.tilesPlayed.push(computerChoice);
+                    tileChoice.textContent = gameOperations.computer.marker;
+                    gameOperations.gameOver();
+                    }
+                }
             }
         })();
-    },
+    })(),
 
     winningCombos: [123, 456, 789, 147, 258, 369, 159, 357],
 
-    player: {
-        name: "Player1",
-        marker: "X",
-        tilesPlayed: [],
+    Player: function(name, marker) {
+        this.name = name;
+        this.marker = marker;
+        this.tilesPlayed = [];
     },
-
 }
 
-const gameOperations = {
-    markTile: function(tileNum) {
-        gameModule.player.tilesPlayed.push(tileNum);
-    },
-    gameover: function() {
-        gameModule.player.tilesPlayed.sort();
-        tilesPlayedString = gameModule.player.tilesPlayed.join("");
-        
-        gameModule.winningCombos.forEach((wincombo) => {
-            if (tilesPlayedString.includes(wincombo)) {
-                alert(gameModule.player.name + "is the winner!")
 
-            }
-    })},
-    computerTurn: function() {
-        let computerChoice = Math.trunc(Math.random()*9) + 1;
+const gameOperations = {
+    roundCount: 0,
+    
+    player1: new gameModule.Player("player1", "X"),
+    
+
+    computer: new gameModule.Player("computer", "O"),
+
+    gameOver: function() {
+        this.player1.tilesPlayed.sort();
+        playerTilesPlayedString = this.player1.tilesPlayed.join("");
+        console.log(playerTilesPlayedString);
         
-    }
+        this.computer.tilesPlayed.sort();
+        computerTilesPlayedString = this.computer.tilesPlayed.join("");
+
+        function clearTilesPlayed() {
+            gameOperations.player1.tilesPlayed = [];
+            gameOperations.computer.tilesPlayed = [];
+        }
+
+        const gameTiles = document.querySelectorAll(".gameTile")
+        const regex = /(1.*2.*3|4.*5.*6|7.*8.*9|1.*4.*7|2.*5.*8|3.*6.*9|1.*5.*9|3.*5.*7)/;
+        const playerMatches = playerTilesPlayedString.match(regex);
+        const computerMatches = computerTilesPlayedString.match(regex);
+        if (playerMatches) {
+            alert(this.player1.name + " is the winner!");
+            gameTiles.forEach((tile) => {
+                tile.textContent = "";
+            })
+            clearTilesPlayed();
+        } else if (computerMatches) {
+            alert(this.computer.name + " is the winner!")
+            gameTiles.forEach((tile) => {
+                tile.textContent = "";
+            })
+            clearTilesPlayed();
+        }
+    },
 };
